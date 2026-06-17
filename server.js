9e59e86de0561b33806d5393d07a8202721d81f3
Y <patipraveen01@gmail.com>
2026-06-17 12:29:34 +0530

    put

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('').then(
+mongoose.connect('mongodb+srv://loginsa80_db_user:eKOnSFD1e7DKyTLO@cluster0.iubw3xb.mongodb.net/').then(
     ()=> console.log('conneted to database......')
 ).catch(err => console.log(err))
 
@@ -24,6 +24,17 @@ app.post('/add_user', async (req,res)=>{
     }
 })
 
+app.put('/update/:id', async (req,res)=>{
+    const {username} = req.body;
+    const {email} = req.body;
+    try{
+        await UserData.findByIdAndUpdate(req.params.id,{username,email},{new:true});
+        return res.json({message: "user data updated successfully"})
+    }
+    catch(err){
+        console.log(err.message);
+    }
+});
 
 
 app.listen(3000,()=>console.log('server running on http://127.0.0.1:3000.....'))
